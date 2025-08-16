2a8af18244d76b04382b5ef3ee706e46ce443006
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-08-17 00:13:18 +0530

    basics

--- Complete C++/Basics.cpp
+++ Complete C++/Basics.cpp
@@ -1,6 +1,15 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     cout << "Hello, World!" << endl;
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(){
+    cout<<"Hello prakash" ;
+    cout<<endl;
+    cout<<endl;
+    cout<<"Hello Prakash Jha" ;
 }
