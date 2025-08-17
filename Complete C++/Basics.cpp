a2322cc230022fb40192731f57dc57c5b984ee63
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-08-17 22:39:25 +0530

    basics

--- Complete C++/Basics.cpp
+++ Complete C++/Basics.cpp
@@ -1,9 +1,9 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     cout << "Hello, World!" << endl;
-//     return 0;
-// }
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
 
 #include <iostream>
 using namespace std;
@@ -13,3 +13,106 @@ int main(){
     cout<<endl;
     cout<<"Hello Prakash Jha" ;
 }
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x ;
+    x = 29;           
+    int y ;
+    y =22;
+    cout << x*y ;
+}
+
+
+//  int x = 5, y= 10;
+// cout << x*y;  , so its output will be 50
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5;
+    cout << x << endl;
+    x = 7;
+    cout << x << endl;
+    x = x +6;
+    cout << x << endl;
+    x = x - 20 ;
+    cout << x << endl;
+}
+
+// Arithmetic Operatons on int data types-----
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5, y = 2;
+    cout << x+y << endl;   // addition 
+    cout << x-y << endl; // substraction
+    cout << x*y << endl; // multiplication
+    cout << x/y << endl; // division
+}
+
+
+// Fload Data Types : 
+
+#include <iostream>
+using namespace std;
+int main() {
+    float x = 3.14, y = 2.6;
+    cout << x+y << endl;   // addition 
+    cout << x-y << endl; // substraction
+    cout << x*y << endl; // multiplication
+    cout << x/y << endl; // division
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 4;
+    cout << x << endl;
+    x++;
+    cout << x << endl;
+    x--;
+    cout << x << endl;
+    ++x;
+    cout << x << endl;
+    --x;
+    cout << x << endl;
+}
+
+
+//  Example : Calculating area of a Cricle
+
+#include <iostream>
+using namespace std;
+int main() {
+    float r = 4.3;
+    float a = 3.1415*r*r;
+    cout << a;
+}
+
+// Variable Naming Rules in c++;
+// variable can start form an alphabet or underscore _ or $
+
+ #include <iostream>
+ using namespace std;
+ int main() {
+    int _1 = 3;
+    cout << _1;
+ }
+
+
+// user input and output in 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cin >> x;
+    cout << x*x;
+}
+
+
