cd83a33d23e6d9df7a934010672b1fffb6d81200
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-08-20 02:40:39 +0530

    conditionals

--- Complete C++/Conditionals.cpp
+++ Complete C++/Conditionals.cpp
@@ -56,3 +56,114 @@ int main (){
         cout << "No profit no Loss";
     }
 }
+
+//  Three digit or not 
+
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout << "enter n : ";
+    cin >> n;
+    if (n>99 and n<1000) {
+        cout << "Three digit number";
+    }
+    else {
+        cout << "not a Three digit number";
+    
+    }
+}
+
+// Take 3 number intput and tell  if they can be teh sides of a tringle
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b ,c ;
+    cout << "Enter three sides of a triangle: " << endl;
+    cout << "Enter first side: ";
+    cin >> a;
+    cout << "Enter second side: ";
+    cin >> b;
+    cout << "Enter third side: ";
+    cin >> c;
+    if (a+b>c and a+c>b and b+c>a){
+        cout << "These sides can form a triangle";
+    }
+    else {
+        cout << "There sides cannot form a triangle";
+    }
+}
+
+
+// Take 3 positive integer input and print the greatest of them.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cout << "Enter three positive integers: " << endl;
+    cout << "Enter first integer: ";
+    cin >> a;
+    cout << "Enter second integer: ";
+    cin >> b;
+    cout << "Enter third integer: ";
+    cin >> c;
+
+    if (a >= b && a >= c) {
+        cout << "Greatest number is: " << a;
+    } else if (b >= a && b >= c) {
+        cout << "Greatest number is: " << b;
+    } else {
+        cout << "Greatest number is: " << c;
+    }
+}
+
+
+//  Take 3 positive integers input and print the least of them.
+
+#include <iostream>
+using namespace std;
+int main (){
+    int a, b, c;
+    cout << "Enter three Positive Integers: " << endl;
+    cout << "Enter first Integer: ";
+    cin >> a;
+    cout << "Enter second Interger: ";
+    cin >> b;
+    cout << "Enter third Integer: " ;
+    cin >> c;
+    if (a <= b and a <= c) {
+        cout << "Least number is: " << a;
+    }
+    else if (b <= a and b <= c) {
+        cout << "Least number is: " << b;
+
+    }
+    else {
+        cout << "Least number is : " << c;
+
+    }
+}
+
+// Take positive integer input and tell if it is divisible by 5 or 3 but not divisible by 15
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a positive integr: ";
+    cin >> n;
+    if ((n % 5 == 0 or n % 3 == 0) and n % 15 != 0) {
+        cout << "The number is divisible by 5 or 3 but not divisible by 15";
+
+    }
+    else {
+        cout << "The number is not divisible by 5 or 3 or it is divisible by 15 " << endl;
+
+
+    }
+}
+
+// Take 3 positive integer input and print the greatest of them.
+
