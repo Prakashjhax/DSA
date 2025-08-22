3fee137afdd14f717da88eb106d1f3d7558ee03c
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-08-23 00:56:58 +0530

    Pattern

---
+++ Complete C++/pattern.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "*****"<<endl;
+    cout<< "*****"<<endl;
+    cout<< "*****"<<endl;
+}
+
+// rectangle banana hai kuch aise ki meri no. of rows jo hai variable hai 
+
+// rows -> m, cols -> 5
+
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the no. of rows: ";
+    cin>>m;
+    int n;
+    cout << "Enter number of cols : ";
+    cin>>n;
+    for (int  i=1; i<=m; i++){
+        for (int i=1;i<=n; i++){
+            cout<<" * ";
+        }
+        cout << endl;
+    }
+}
+
+
+// print the given pattern 
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<" * * * *"<<endl;
+    cout<<" * * * * "<<endl;
+    cout<<" * * * * "<<endl;
+    cout<<" * * * * "<<endl;
+}
+
+
+#include<Iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of rows : ";
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=n; j++){
+            cout <<" p ";
+        } 
+        cout<<endl;
+    }
+    
+}
+
+#include<Iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout << "Emter rows and cols: ";
+    cin>> m >> n ;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=m; j++){
+            cout<<"$";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+// Print the given Pattern 
+
+#include<iostream>
+using namespace std;
+int n;
+cout<<"Enter the no. of rows : "
+int m;
+cout << "Enter the no. of cols : "
+for ()
