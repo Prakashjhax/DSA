ef6d6188e0c030b05e2211aacf33056c40f1f575
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-08-25 00:07:34 +0530

    Pattern

--- Complete C++/pattern.cpp
+++ Complete C++/pattern.cpp
@@ -320,5 +320,67 @@ int main(){
 
 // Floyd's Triangle
 
+Print the given Pattern
+
+1
+23
+456
+78910
+
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<< "Enter number of rows : ";
+    cin >> n;
+    int a=1;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+    }
+}
+
+// 0 and 1 Triangle ;
+
+// 1
+// 01
+// 101
+// 0101
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter number of rows : ";
+    cin>> n;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            if ((i+j)%2 ==0) cout << 1<<" ";
+            else cout<<0<<" ";
+        }
+        cout << endl;
+    }
+}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    int mid = n/2 + 1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==mid || j==mid) cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
 
 
