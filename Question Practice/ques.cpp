1525a49dac3d73aa1f0e6c9a0e33a3b2bf02cbd5
user.name : "jhaprakashkumar122@gmail.com <jhaprakashkumar122@gmail.com>
2025-09-04 22:22:10 +0530

    Question Practice

--- Question Practice/ques.cpp
+++ Question Practice/ques.cpp
@@ -575,3 +575,26 @@ int main(){
 }
 
 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1; i<=5; i++)
+    {
+        for(j=1; j<=7; j++)
+        {
+            if(j>=i && j<=10-i)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+
+        }
+        cout<<"\n";
+    }
+}
+
